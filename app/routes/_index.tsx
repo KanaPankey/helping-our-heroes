31397dfe526190cd588728c5edaf76099ef39999
KanaPankey <86436056+KanaPankey@users.noreply.github.com>
2024-06-03 17:38:49 -0500

    Initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Helping Our Heroes</h1>
+      <p>An app to help the Charleston Digitial Hub BAH fellows</p>
       <ul>
         <li>
           <a
@@ -18,7 +19,7 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            Roadmaps
           </a>
         </li>
         <li>
@@ -27,12 +28,12 @@ export default function Index() {
             href="https://remix.run/tutorials/jokes"
             rel="noreferrer"
           >
-            Deep Dive Jokes App Tutorial
+            Mental help
           </a>
         </li>
         <li>
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Resource Links
           </a>
         </li>
       </ul>
